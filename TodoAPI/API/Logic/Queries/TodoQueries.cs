051925f47bbe4c38ad8fcbeb57eaabdf794a7711
Stefan Matic <stefan.matic@me.com>
2018-01-21 16:15:25 +0100

    Add TodoQueries

---
+++ TodoAPI/API/Logic/Queries/TodoQueries.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using API.Domain.Interfaces;
+using API.Models;
+
+namespace API.Logic.Queries
+{
+    public class TodoQueries : ITodoQueries
+    {
+        public IEnumerable<TodoItem> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TodoItem Get(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
