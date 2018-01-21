7a7b8024cbaefbe7f81cb044cf0f802c47ac0f6c
Stefan Matic <stefan.matic@me.com>
2018-01-21 16:15:41 +0100

    Add TodoCommands

---
+++ TodoAPI/API/Logic/Commands/TodoCommands.cs
@@ -0,0 +1,18 @@
+﻿using API.Domain.Interfaces;
+using API.Models;
+
+namespace API.Logic.Commands
+{
+    public class TodoCommands : ITodoCommands
+    {
+        public int Save(TodoItem Item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Delete(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
