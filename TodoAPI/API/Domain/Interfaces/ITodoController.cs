a92ca4bddcd3b6808c02c2239496c9296425d5dd
Stefan Matic <stefan.matic@me.com>
2018-01-21 15:32:14 +0100

    Add TodoController Interface

---
+++ TodoAPI/API/Domain/Interfaces/ITodoController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using API.Models;
+
+namespace API.Domain.Interfaces
+{
+    public interface ITodoController
+    {
+        IEnumerable<TodoItem> Get();
+        TodoItem Get(int Id);
+        void Post(TodoItem item);
+        void Put(TodoItem item);
+        void Delete(int Id);
+    }
+}
