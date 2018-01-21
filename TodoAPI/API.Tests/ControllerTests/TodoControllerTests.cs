bfa990f6b9b810a9b96d3c697703d2d3cfb571e8
Stefan Matic <stefan.matic@me.com>
2018-01-21 15:31:32 +0100

    Add TodoControllerTests class

---
+++ TodoAPI/API.Tests/ControllerTests/TodoControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using API.Controllers;
+using API.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API.Tests.ControllerTests
+{
+    [TestClass]
+    public class TodoControllerTests
+    {
+        private TodoController _todoController;
+
+        public TodoControllerTests()
+        {
+            _todoController = new TodoController();
+        }
+
+        [TestMethod]
+        [TestCategory("TodoControllerTests")]
+        public void Should_return_list_of_todo_items()
+        {
+            //Arrange
+            List<TodoItem> result;
+            int expected = 1;
+            
+            //Act
+            result = _todoController.Get().ToList();
+            
+            //Assert
+            Assert.AreEqual(result.Count, expected);
+        }
+        
+    }
+}
