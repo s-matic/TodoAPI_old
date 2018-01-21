948c9652057ec0dca26d061526a93e938e4f85e7
Stefan Matic <stefan.matic@me.com>
2018-01-21 13:19:17 +0100

    Add test project

---
+++ TodoAPI/API.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace API.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
